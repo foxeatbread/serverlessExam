47f21e7ea686ada8bfd47143447bfad0792592cc
顾昊轩 <guhaoxuan@guhaoxuandeMBP.wit.ie>
2024-03-22 10:09:14 +0000

    test finish

--- lambdas/getMovieMovieSpAward.ts
+++ lambdas/getMovieMovieSpAward.ts
@@ -25,55 +25,104 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     const awardBody = pathParameters.awardBody;
     const movieId = parseInt(pathParameters.movieId);
 
-    const commandOutput = await ddbDocClient.send(
-        new QueryCommand({
-            TableName: process.env.TABLE_NAME,
-            KeyConditionExpression: "movieId = :movieId",
-            ExpressionAttributeValues: {
-                ":movieId": movieId,
-            },
-        })
-    );
+    if(pathParameters && pathParameters.min){
+        const min = parseInt(pathParameters.min)
+        const commandOutput = await ddbDocClient.send(
+            new QueryCommand({
+                TableName: process.env.TABLE_NAME,
+                KeyConditionExpression: "movieId = :movieId",
+                ExpressionAttributeValues: {
+                    ":movieId": movieId,
+                },
+            })
+        );
 
-    if (!commandOutput.Items) {
-        return {
-            statusCode: 404,
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({ Message: "No awards found for the specified movie" }),
-        };
+        if (!commandOutput.Items) {
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ Message: "No awards found for the specified movie" }),
+            };
+        }
+        const itemCount = commandOutput.Items.length;
+        if (itemCount > min) {
+            const body = {
+              data1: commandOutput.Items,
+            }
+
+            return {
+                statusCode: 200,
+                headers: {
+                  "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                  body,
+                }),
+              };
+        }else{
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ Message: "Request failed" }),
+            };
+        }
+
+        
     }
+    else{
+        const commandOutput = await ddbDocClient.send(
+            new QueryCommand({
+                TableName: process.env.TABLE_NAME,
+                KeyConditionExpression: "movieId = :movieId",
+                ExpressionAttributeValues: {
+                    ":movieId": movieId,
+                },
+            })
+        );
     
-    const items = commandOutput.Items;
-    const filter = items.filter(item => 
-        item.reviewerName.includes(awardBody) || item.reviewDate.includes(awardBody)
-    );
-
-    if (!filter || filter.length === 0) {
-        return {
-            statusCode: 404,
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({ Message: "No awards found for the specified movie" }),
+        if (!commandOutput.Items) {
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ Message: "No awards found for the specified movie" }),
+            };
+        }
+        
+        const items = commandOutput.Items;
+        const filter = items.filter(item => 
+            item.reviewerName.includes(awardBody) || item.reviewDate.includes(awardBody)
+        );
+    
+        if (!filter || filter.length === 0) {
+            return {
+                statusCode: 404,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({ Message: "No awards found for the specified movie" }),
+            };
+        }
+    
+        const body = {
+            data1: filter,
         };
+        return {
+            statusCode: 200,
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({
+              body,
+            }),
+          };
     }
 
-    const body = {
-        data1: filter,
-    };
-
-
-    return {
-      statusCode: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        body,
-      }),
-    };
   } catch (error: any) {
     console.log(JSON.stringify(error));
     return {
--- test/simple-app.test.ts
+++ test/simple-app.test.ts
@@ -1,18 +1,87 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as SimpleApp from '../lib/simple-app-stack';
+import * as cdk from "aws-cdk-lib";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
+import { generateBatch } from "../shared/util";
+import { movies, movieCasts, movieAwards, movieCrew } from "../seed/movies";
+import * as apig from "aws-cdk-lib/aws-apigateway";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/simple-app-stack.ts
-// test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+export class RestAPIStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-// });
+    const movieAwardsTable = new dynamodb.Table(this, "MovieAwardsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "movieId", type: dynamodb.AttributeType.NUMBER },
+      sortKey: { name: "awardBody", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieAwards",
+    });
+ 
 
+    const getMovieMovieSpAwardFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieMovieSpAwardFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getMovieMovieSpAward.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: movieAwardsTable.tableName,
+          REGION: "eu-west-1",
+        },
+      }
+    );
+
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [movieAwardsTable.tableName]: generateBatch(movieAwards),
+
+
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [movieAwardsTable.tableArn]
+      }),
+    });
+
+    const api = new apig.RestApi(this, "RestAPI", {
+      description: "demo api",
+      deployOptions: {
+        stageName: "dev",
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date"],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+    });
+
+
+    const movieAwardsEndpoint = api.root.addResource("awards");
+    const awardsTypeEndpoint = movieAwardsEndpoint.addResource("{awardBody}");
+    const moviesEndpoint = awardsTypeEndpoint.addResource("movies")
+    const movieEndpoint = moviesEndpoint.addResource("{movieId}");
+    movieEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieMovieSpAwardFn, { proxy: true })
+    );
+
+
+    Permissions;
+    movieAwardsTable.grantReadData(getMovieMovieSpAwardFn)
+
+  }
+}
